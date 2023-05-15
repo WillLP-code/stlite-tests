9cda1d13a1ff927979e7dad9e21ec8f90ad7a805
William Levack Payne <108789355+willLPD2I@users.noreply.github.com>
2023-05-15 13:47:22 +0100

    added file upload -working

--- test_app/test.py
+++ test_app/test.py
@@ -1,5 +1,14 @@
 import streamlit as st
+import pandas as pd
 
 st.title("A test of STlite for D2I")
 
+uploaded_files = st.file_uploader('Choose a file', accept_multiple_files=True)
 
+
+for uploaded_file in uploaded_files:
+    st.write('filename:', uploaded_file.name)
+    df = pd.read_csv(uploaded_file)
+
+if uploaded_files:
+    st.dataframe(df)
