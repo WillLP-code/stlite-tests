63f38f1d7e560173af6be3999a0769c2809bf62e
William Levack Payne <108789355+WillLP-code@users.noreply.github.com>
2023-06-02 09:36:26 +0100

    added years slider to bench test

--- benchmarking test/bench_test.py
+++ benchmarking test/bench_test.py
@@ -1,32 +1,41 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+from pyodide.http import open_url
 
 
 
-uploaded_file = st.file_uploader('Upload a1 data here:', accept_multiple_files=False)
-if uploaded_file:
-        df = pd.read_csv(uploaded_file)
+data = open_url("https://raw.githubusercontent.com/WillLP-code/stlite-tests/main/benchmarking%20test/data/a1.csv")
+df = pd.read_csv(data)
         
-        category = df['category'].unique()        
-        
+category = df['category'].unique()        
 
-        with st.sidebar:
-            cat_option = st.sidebar.selectbox(
-            'What data category would you like?',
-            (category))
-            st.write('You selected:', cat_option)
-        df = df[df['category'] == cat_option]
-        
-        with st.sidebar:
-            category_type = df['category_type'].unique()
-            cat_type_option = st.sidebar.selectbox(
-            'What sub-category would you like?',
-            (category_type))
-            st.write('You selected:', cat_type_option)
-        df = df[df['category_type'] == cat_type_option]
-        st.dataframe(df)
+with st.sidebar:
+        years = st.sidebar.slider('Year select',
+                        min_value=2013,
+                        max_value=2022,
+                        value=[2013,2022])
 
-        fig = px.line(df, x="time_period", y="number", title=f'{cat_option} : {cat_type_option}')
-        st.plotly_chart(fig)
+df = df[(df['time_period'].astype(int) >= years[0]) & (df['time_period'].astype(int) <= years[1])]
+
+with st.sidebar:
+    cat_option = st.sidebar.selectbox(
+    'What data category would you like?',
+    (category))
+    st.write('You selected:', cat_option)
+df = df[df['category'] == cat_option]
+
+with st.sidebar:
+    category_type = df['category_type'].unique()
+    cat_type_option = st.sidebar.selectbox(
+    'What sub-category would you like?',
+    (category_type))
+    st.write('You selected:', cat_type_option)
+df = df[df['category_type'] == cat_type_option]
+st.dataframe(df)
+
+
+
+fig = px.line(df, x="time_period", y="number", title=f'{cat_option} : {cat_type_option}')
+st.plotly_chart(fig)
 
