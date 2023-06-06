37a36061a131fe2b5c82c634cb25f99ac98b8086
William Levack Payne <108789355+WillLP-code@users.noreply.github.com>
2023-06-06 15:20:42 +0100

    added sankey

--- sankey_processing_test/process.py
+++ sankey_processing_test/process.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import datetime
+import plotly.graph_objects as go
 
 '''
 The csvs must start with the first row as the header, then the data
@@ -13,6 +14,47 @@ referrla nfa colum must be referral nfa?
 case status must be called case status
 '''
 
+names = ['assessment_nfa',
+        'cin_start',
+        'cpp_start',
+        'lac_start',
+        'last_status_assessment_authorised',
+        'last_status_assessment_nfa',
+        'last_status_cin_end',
+        'last_status_cin_start',
+        'last_status_contact',
+        'last_status_cpp_end',
+        'last_status_cpp_start',
+        'last_status_early_help_assessment_end',
+        'last_status_icpc',
+        'last_status_lac_end',
+        'last_status_lac_start',
+        'last_status_s47',
+        'referral',
+        'referral_nfa',
+        ]
+labels = ['Assessment: No further action', 
+        'Child in need plan', 
+        'Child protection plan', 
+        'Looked after child', 
+        'End: Assessment', 
+        'End: No further action', 
+        'End: Child in need plan ends', 
+        'End: Child in need plan', 
+        'End: Contact', 
+        'End: Child protection plan ends',
+        'End: Child protection plan', 
+        'End: Early help ends?', 
+        'End: Initial child protection conference', 
+        'End: Looked after status ends', 
+        'End: Looked after child', 
+        'End: Section 47 assessment', 
+        'Referral',
+        'Referral: No further action',
+        ]
+
+labs = pd.DataFrame(list(zip(names, labels)),
+                            columns=['name', 'lab'])
 
 st.title('test')
 journey_events = {'contact': {'contacts':'contact_dttm'},
@@ -381,10 +423,50 @@ if uploaded_files:
     journey = journey[["target", "source", "ref_id"]]
 
     # collapse data frame 
-    df_coll = journey.groupby(['target', 'source']).count().reset_index()
+    df = journey.groupby(['target', 'source']).count().reset_index()
 
-    #output data for SANKEY
-    # Save to Excel
+    #####################################
+    # Sankey
+    #####################################
+    df = df.merge(labs, left_on = "target", right_on = "name")
+    df = df.rename(columns = {'lab':'target_lab'})
+    df = df.merge(labs, left_on = "source", right_on = "name")
+    df = df.rename(columns = {'lab':'source_lab'})
+    df = df.drop(columns = ["name_x", "target", "source", "name_y"])
+    df = df.rename(columns = {'target_lab':'target','source_lab':'source'})
+    source_string = df["source"].values.tolist()
+    target_string = df["target"].values.tolist()
+
+     # create index values for source and target 
+    combo = source_string + target_string
+    all_options = np.unique(combo)
+    options_to_merge = pd.DataFrame(all_options, columns = ["options"])
+    options_to_merge.reset_index(inplace=True)
+    options_to_merge = options_to_merge.rename(columns = {'index':'sankey_index'})
+    labels = options_to_merge["options"].values.tolist()
+
+
+    # first merge the index values for source
+    df_ind = df.merge(options_to_merge, left_on = "source", right_on = "options")
+    # rename and drop so we can re-merge
+    df_ind = df_ind.rename(columns = {"sankey_index":"source_index"})
+    df_ind = df_ind.drop(columns = "options")
+    # merge the index values for target 
+    df_ind = df_ind.merge(options_to_merge, left_on = "target", right_on = "options")
+    df_ind = df_ind.rename(columns = {"sankey_index":"target_index"})
+
+    #turn columns into arrays so we can create a dictionary for the Sankey input
+    source = df_ind["source_index"].values.tolist()
+    target = df_ind["target_index"].values.tolist()
+    value  = df_ind["ref_id"].values.tolist()
+
+    link = dict(source = source, target = target, value = value)
+    node = dict(label = labels, pad = 15, thickness = 5)
+
+    
+    data = go.Sankey(link = link, node = node)
+    fig = go.Figure(data)
+    st.plotly_chart(fig)
   
 
     st.write(df.astype(object))
